786591ac1f91c2efdb31d8ffd9e88023184ea124
yangzhitao <yangzhitao@sensetime.com>
2026-07-24 01:29:28 +0000

    chore: run release service from start script

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ export OMNI_BACKEND_URL=${OMNI_BACKEND_URL:-http://127.0.0.1:28080}
 export CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-/tmp/cargo_build_target}
 
 build_service() {
-    cargo build --release --bin omni-stream
+    cargo build --release --bin omni-stream --features duckdb
 }
 
 build_frontend() {
@@ -17,12 +17,14 @@ build_frontend() {
 }
 
 build_all() {
-    build_service
     build_frontend
+    build_service
 }
 
 run_service() {
-    RUST_LOG=${RUST_LOG:-info,tower_http=debug} cargo run --bin omni-stream
+    local service_bin="${CARGO_TARGET_DIR:-./target}/release/omni-stream"
+    [[ -x "$service_bin" ]] || build_all
+    RUST_LOG=${RUST_LOG:-info,tower_http=debug} "$service_bin"
 }
 
 run_frontend() {
